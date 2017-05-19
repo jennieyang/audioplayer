5d751899addc8f17f887dd3dc19ce422c6fbfb9f
Jenn <yangyjennie@gmail.com>
2017-05-18 23:44:36 -0500

    Add reference to media control buttons

--- app/src/main/java/com/jennieyang/audioplayer/MusicPlayerActivity.java
+++ app/src/main/java/com/jennieyang/audioplayer/MusicPlayerActivity.java
@@ -1,13 +1,32 @@
 package com.jennieyang.audioplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MusicPlayerActivity extends AppCompatActivity {
+    private ImageButton btnPlay;
+    private ImageButton btnForward;
+    private ImageButton btnBackward;
+    private ImageButton btnNext;
+    private ImageButton btnPrevious;
+    private TextView songTitleLabel;
 
+    private MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player);
+
+        btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+        btnForward = (ImageButton) findViewById(R.id.btnForward);
+        btnBackward = (ImageButton) findViewById(R.id.btnBackward);
+        btnNext = (ImageButton) findViewById(R.id.btnNext);
+        btnPrevious = (ImageButton) findViewById(R.id.btnPrevious);
+        songTitleLabel = (TextView) findViewById(R.id.songTitle);
+
+        mp = new MediaPlayer();
     }
 }
